f1619145df2cde6d4879f8fa277e30ddab412db8
MarcoFabretti <mfabretti2000@gmail.com>
2024-01-18 17:53:32 +0100

    Main eureka server

---
+++ ecommerc/src/test/java/com/vantea/ecommerce/ecommerc/EcommercApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vantea.ecommerce.ecommerc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
